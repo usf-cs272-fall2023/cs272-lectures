81fd2319db23d3e4d7805cf8a7b880c2e626b667
Sophie Engle <sjengle@cs.usfca.edu>
2023-02-12 17:49:12 -0800

    Adding templates

---
+++ ExploringObjects/templates/PrefixMap.java
@@ -0,0 +1,11 @@
+package edu.usfca.cs272;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class PrefixMap {
+	private final TreeMap<String, TreeSet<String>> data;
+}
