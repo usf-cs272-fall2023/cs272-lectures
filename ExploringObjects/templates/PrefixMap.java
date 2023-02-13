bc8714983e51fbf1a1ebbbf84781ae31ec7a350c
Sophie Engle <sjengle@cs.usfca.edu>
2023-02-12 17:50:56 -0800

    Adding more skeleton to template

--- ExploringObjects/templates/PrefixMap.java
+++ ExploringObjects/templates/PrefixMap.java
@@ -1,11 +1,16 @@
 package edu.usfca.cs272;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PrefixMap {
 	private final TreeMap<String, TreeSet<String>> data;
+
+	public PrefixMap(int prefixSize) {
+		this.data = new TreeMap<>();
+	}
+
+	public PrefixMap() {
+		this(1);
+	}
 }
